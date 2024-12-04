fabfa4c7bec79e541b984076b7b940d6a8a80829
João Victor Moreira <jvrodriguesmoreira@gmail.com>
2024-12-04 19:47:08 -0300

    Add articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import articles from "@/app/data/articles.json";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
@@ -7,77 +10,56 @@ export default function Home() {
 
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
-          <div className="flex-center bg-indigo-500"> AA </div>
-          <div className="flex-center bg-indigo-500"> bb </div>
-          <div className="flex-center bg-indigo-500"> cc </div>
-          <div className="flex-center bg-indigo-500"> dd </div>
+          {articles.splice(-4).map((article) => (
+            <div
+              key={article.title}
+              className="flex-center relative overflow-hidden transition duration-500 hover:scale-105 cursor-pointer"
+            >
+              <div className="h-full w-full">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={`/assets/images/articles/${article.image}`}
+                  alt={article.image_description}
+                  width={600}
+                  height={400}
+                />
+              </div>
+              <p className="absolute bottom-0 pt-6 pb-2 px-2 bg-gradient-to-t from-slate-900 via-slate-800 to-transparent w-full">
+                {article.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
 
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-8  flex gap-4 flex-col">
-            <div className="flex gap-4 bg-blue-800 rounded-md overflow-hidden">
-              <div>
-                <img
-                  className="w-auto h-[200px]"
-                  src="https://placehold.co/600x400"
-                  alt="image description"
-                />
+          <div className="col-span-8 flex gap-4 flex-col">
+            {articles.map((article) => (
+              <div
+                key={article.title}
+                className="flex gap-4 bg-slate-800 rounded-md overflow-hidden transition duration-500 hover:scale-105 cursor-pointer "
+              >
+                <div>
+                  <Image
+                    className="h-full w-full object-cover"
+                    src={`/assets/images/articles/${article.image}`}
+                    alt={article.image_description}
+                    width={300}
+                    height={400}
+                  />
+                </div>
+                <div className="flex flex-1 flex-col gap-2 py-4">
+                  <h2 className="text-3xl text-indigo-400">{article.title}</h2>
+                  <p className="flex-grow">{article.excerpt}</p>
+                  <button className="bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max">
+                    Ler mais
+                  </button>
+                </div>
               </div>
-              <div className="flex flex-1 flex-col gap-2 py-4">
-                <h2 className="text-3xl">Algum titulo</h2>
-                <p className="flex-grow">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-                  ullam rem asperiores debitis quae dolores voluptates maiores,
-                  mollitia voluptate, accusamus dolorum ea laborum! Hic nesciunt
-                  deleniti minima exercitationem! Fuga, maiores?
-                </p>
-                <button>Ler mais</button>
-              </div>
-            </div>
-
-            <div className="flex gap-4 bg-blue-800 rounded-md overflow-hidden">
-              <div>
-                <img
-                  className="w-auto h-[200px]"
-                  src="https://placehold.co/600x400"
-                  alt="image description"
-                />
-              </div>
-              <div className="flex flex-1 flex-col gap-2 py-4">
-                <h2 className="text-3xl">Algum titulo</h2>
-                <p className="flex-grow">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-                  ullam rem asperiores debitis quae dolores voluptates maiores,
-                  mollitia voluptate, accusamus dolorum ea laborum! Hic nesciunt
-                  deleniti minima exercitationem! Fuga, maiores?
-                </p>
-                <button>Ler mais</button>
-              </div>
-            </div>
-
-            <div className="flex gap-4 bg-blue-800 rounded-md overflow-hidden">
-              <div>
-                <img
-                  className="w-auto h-[200px]"
-                  src="https://placehold.co/600x400"
-                  alt="image description"
-                />
-              </div>
-              <div className="flex flex-1 flex-col gap-2 py-4">
-                <h2 className="text-3xl">Algum titulo</h2>
-                <p className="flex-grow">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-                  ullam rem asperiores debitis quae dolores voluptates maiores,
-                  mollitia voluptate, accusamus dolorum ea laborum! Hic nesciunt
-                  deleniti minima exercitationem! Fuga, maiores?
-                </p>
-                <button>Ler mais</button>
-              </div>
-            </div>
+            ))}
           </div>
-          <div className="col-span-4"> bb </div>
+          <div className="col-span-4 bg-green-800"> bb </div>
         </div>
       </div>
     </>
