ae853919376aceb24e70876103d67d1d0b0f641b
João Victor Moreira <jvrodriguesmoreira@gmail.com>
2024-12-05 19:19:29 -0300

    Fetching articles from database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import articles from "@/data/articles.json";
 import Image from "next/image";
+import Article from "./libs/database/Articles";
 
-export default function Home() {
+export default async function Home() {
+  const articles = await Article.get({
+    orderBy: { publishedAt: "desc" },
+    limit: 14,
+  });
+  const highlightedArticles = articles.slice(0, 4);
+  const listArticles = articles.slice(4);
+
   return (
     <>
       <div className="w-full h-[35vh] bg-orange-400 flex-center">
@@ -10,7 +17,7 @@ export default function Home() {
 
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
-          {articles.splice(-4).map((article) => (
+          {highlightedArticles.map((article) => (
             <div
               key={article.title}
               className="flex-center relative overflow-hidden transition duration-500 hover:scale-105 cursor-pointer"
@@ -19,7 +26,7 @@ export default function Home() {
                 <Image
                   className="h-full w-full object-cover"
                   src={`/assets/images/articles/${article.image}`}
-                  alt={article.image_description}
+                  alt={article.title}
                   width={600}
                   height={400}
                 />
@@ -35,7 +42,7 @@ export default function Home() {
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-8 flex gap-4 flex-col">
-            {articles.map((article) => (
+            {listArticles.map((article) => (
               <div
                 key={article.title}
                 className="flex gap-4 bg-slate-800 rounded-md overflow-hidden transition duration-500 hover:scale-105 cursor-pointer "
@@ -44,13 +51,22 @@ export default function Home() {
                   <Image
                     className="h-full w-full object-cover"
                     src={`/assets/images/articles/${article.image}`}
-                    alt={article.image_description}
-                    width={300}
-                    height={400}
+                    alt={article.title}
+                    width={350}
+                    height={200}
                   />
                 </div>
                 <div className="flex flex-1 flex-col gap-2 py-4">
                   <h2 className="text-3xl text-indigo-400">{article.title}</h2>
+                  <p className="flex-grow">
+                    <small>
+                      {article.publishedAt.toLocaleString("pt-BR", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </small>
+                  </p>
                   <p className="flex-grow">{article.excerpt}</p>
                   <button className="bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max">
                     Ler mais
