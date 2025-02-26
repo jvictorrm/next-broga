07c4dcd660d43da72b53e00009eaebb1af1d4c5f
João Victor Moreira <jvrodriguesmoreira@gmail.com>
2025-02-26 16:50:02 -0300

    keep user logged in

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,21 @@
-const User = () => {
-  return <h1>User</h1>;
+import { getSession } from "@/helpers/session";
+import { redirect } from "next/navigation";
+
+const User = async () => {
+  const user = await getSession();
+  if (!user) redirect("/auth/sign-in");
+
+  return (
+    <div className="container mx-auto my-6">
+      <div className="w-2/3">
+        <h3 className="text-2xl my-6">Account</h3>
+        <div className="my-6 flex flex-col">
+          <p className="my-2">{user.name}</p>
+          <p className="my-2">{user.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default User;
--- src/helpers/session.ts
+++ src/helpers/session.ts
@@ -1,15 +1,37 @@
 import { cookies } from "next/headers";
-import { decrypt } from "./jwt";
+import { decrypt, encrypt } from "./jwt";
+
+const SESSION_NAME = "session";
+const generateExpires = () => new Date(Date.now() + 60 * 1000);
 
 export const createSession = (payload: string) => {
-  const expires = new Date(Date.now() + 60 + 1000);
-  cookies().set("session", payload, { expires, httpOnly: true });
+  cookies().set(SESSION_NAME, payload, {
+    expires: generateExpires(),
+    httpOnly: true,
+  });
 };
 
 export const getSession = async () => {
-  const session = cookies().get("session")?.value;
+  const session = cookies().get(SESSION_NAME)?.value;
 
   if (!session) return null;
 
   return await decrypt(session);
+};
+
+export const updateSession = async () => {
+  const session = await getSession();
+  if (!session) return null;
+
+  const expires = generateExpires();
+  const jwt = await encrypt({ ...session, expires });
+
+  const updateSession = {
+    name: SESSION_NAME,
+    value: jwt,
+    expires,
+    httpOnly: true,
+  };
+
+  return updateSession;
 };
